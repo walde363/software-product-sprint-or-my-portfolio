f03801291caf9ac7d3bf00a0f0b4e919da94c84d
Waldemar Ramos Colon <ramoswaldemar082@gmail.com>
2021-06-03 18:07:59 +0000

    Change random messages

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Detective!!!!!!', 'I play with Legos', '9 its my favorite movie', 'I got the eyebrow piercing'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
